d06c9c2ed8a960181d79437e480dc36d05d76a1d
thismain <72316345+thismain@users.noreply.github.com>
2024-09-20 11:39:52 -0700

    Add files via upload

---
+++ blank.js
@@ -0,0 +1,14 @@
+
+
+/*
+because javascript is not listed by github as a language used unless I include a .js file in the repository, apparently
+*/
+
+function toProveThatJavascriptIsBeingUsed(){
+for(let i=0;i<10;i++){
+console.log('fishbones'+i);
+
+}
+
+
+}
