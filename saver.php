226fe85498731b4687bff3f8bfe44a6b383ad2c9
thismain <72316345+thismain@users.noreply.github.com>
2025-02-17 16:13:01 -0800

    Add files via upload

--- saver.php
+++ saver.php
@@ -3,44 +3,12 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
-$graphData="";
-$saving="";
-$loading="";
-$dataFileName="default.txt";
-
-if(isset($_GET['graphData'])){$graphData=$_GET['graphData'];}else{$graphData='';}
-
-if(isset($_GET['saving'])){$saving=$_GET['saving'];}
-else{$saving='';}
-
-if(isset($_GET['loading'])){$loading=$_GET['loading'];}
-else{$loading='';}
+$dir="../graph2/data/";
+$dataFileName="pentagon.txt";
+$graphData=file_get_contents("php://input"); 
 
 if(isset($_GET['dataFileName'])){$dataFileName=$_GET['dataFileName'];}
-else{$dataFileName='default.txt';}
+else{$dataFileName='pentagon.txt';}
 
-
-if($saving){
-
-$graphDataFile=fopen("/storage/emulated/0/Venter/HopWeb/Projects/graph/data/".$dataFileName, "w") or die("Unable to open file!");
-fwrite($graphDataFile, $graphData);
-fclose($graphDataFile);
-}//saving
-
-
-if($loading){
-$graphDataFile=fopen("/storage/emulated/0/Venter/HopWeb/Projects/graph/data/".$dataFileName, "r") or die("Unable to open file!");
-$graphData=fread($graphDataFile,filesize("data/".$dataFileName));
-fclose($graphDataFile);
-}//loading
-
-echo $graphData;
+file_put_contents($dir.$dataFileName,$graphData);
 ?>
-<html>
-<head></head>
-<body style="font-size:40px;font-family:monospace;overflow-vertical:scroll;";>
-<script>
-dataSend='<?php echo $graphData?>';
-</script>
-</body>
-</html>
